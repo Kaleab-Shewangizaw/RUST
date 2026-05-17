460c5a6ad0e572c9d546e6b13af459ae70acf5d2
Kaleab-Shewangizaw <kaleab.stk@gmail.com>
2026-05-17 19:39:12 +0300

    Add incremental compilation artifacts and debug files for ownership_and_borrowing project
    - Created new work-products.bin for incremental compilation.
    - Added lock file for incremental compilation state.
    - Included the main binary for the ownership_and_borrowing project.
    - Generated debug information file linking to the main source file.

---
+++ ownership_and_borrowing/target/debug/deps/ownership_and_borrowing-87d5cc4de46bdc09.d
@@ -0,0 +1,5 @@
+/home/kaleab/Documents/learning/RUST/ownership_and_borrowing/target/debug/deps/ownership_and_borrowing-87d5cc4de46bdc09.d: src/main.rs
+
+/home/kaleab/Documents/learning/RUST/ownership_and_borrowing/target/debug/deps/ownership_and_borrowing-87d5cc4de46bdc09: src/main.rs
+
+src/main.rs:
---
+++ ownership_and_borrowing/target/debug/ownership_and_borrowing.d
@@ -0,0 +1,1 @@
+/home/kaleab/Documents/learning/RUST/ownership_and_borrowing/target/debug/ownership_and_borrowing: /home/kaleab/Documents/learning/RUST/ownership_and_borrowing/src/main.rs
