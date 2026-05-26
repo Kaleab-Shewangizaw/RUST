79c47c8b39d37db3d9a63ec2165ec37c0094e55a
Kaleab-Shewangizaw <kaleab.stk@gmail.com>
2026-05-26 14:59:10 +0300

    refactor: implement command loop in backend registry CLI

--- projects/backend_registry_cli/src/main.rs
+++ projects/backend_registry_cli/src/main.rs
@@ -1,12 +1,31 @@
+use std::io::{self, Write}; 
+
+
 fn backend_registry_cli() {
-    println!("Hello, world!");
+    loop {
+        print!("> ");
+        io::stdout().flush().expect("Failed to flush stdout");
+        
+        let mut command = String::new();
+
+        io::stdin()
+            .read_line(&mut command)
+            .expect("can not read the command!");
+
+        let command = command.trim();
+
+        if command.eq_ignore_ascii_case("exit") {
+            break;
+        }
+    }
 }
 
 
 
-fn main(){
-    println!("welcome,");
-    println!("manage your servers easily!");
+
+fn main() {
+    println!("\nwelcome,");
+    println!("manage your servers easily!\n");
 
     backend_registry_cli();
 }
